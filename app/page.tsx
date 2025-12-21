b9e06d32b3d3e3db469568751fc700f026f13e91
sahadul <sahadul80@gmail.com>
2025-12-21 17:02:05 +0600

    website deployed for demo router problem solved

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       <Hero 
         title={'This Resource Is Currently Under Construction'} 
         content={'All the data are dummy; and for evaluation & Feedback purpuses.'} 
-        buttonText={'Products Divisions'} 
-        buttonLink={'/products-division'} 
+        buttonText={'Product Divisions'} 
+        buttonLink={'/product-divisions'} 
         stats={[
           {number:"144",label:"Partner Factories"},
           {number:"4",label:"Regional Offices"},
